883423f0c23922e9d4811dd062a46f6b54054050
Juan Negrier <juannegrier@gmail.com>
2020-06-30 22:12:22 +0100

    Launch ID numeric validation

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,7 @@ class DeltaService {
 
     var launchId: string = process.env.DELTA_LAUNCH_ID;
 
-    if (!launchId || launchId == 'None') {
+    if (!launchId || isNaN(Number(launchId))) {
       log.info('No Launch detected, creating a new one...');
       var date = new Date();
       var name =
